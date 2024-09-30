6229fe60bfd267e645e69737408231f01200866a
ageria20 <ageria20@gmail.com>
2024-09-30 09:45:58 +0200

    Initial commit

---
+++ src/main/java/ageria/nagefy/NagefyApplication.java
@@ -0,0 +1,13 @@
+package ageria.nagefy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagefyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagefyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ageria/nagefy/NagefyApplicationTests.java
@@ -0,0 +1,13 @@
+package ageria.nagefy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagefyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
